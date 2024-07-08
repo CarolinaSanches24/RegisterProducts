63017a99c857c46022cf9f0bdb3d404da5f748ed
CAROL SANCHES <135077050+CarolinaSanches24@users.noreply.github.com>
2024-07-08 17:57:07 -0300

    :construction: create use state hook

---
+++ httpReact/src/hooks/useFetch.tsx
@@ -0,0 +1,19 @@
+// Custom Hook
+
+import { useEffect, useState } from "react";
+
+export const useFetch = (url:string) => {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const res = await fetch(url);
+            const json = await res.json();
+            setData(json);
+        };
+
+        fetchData();
+    }, [url]);
+
+    return { data };
+};
