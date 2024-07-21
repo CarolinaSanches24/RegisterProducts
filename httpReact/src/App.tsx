5dffc029012085c0a0c94abd9efa8f8b62e7d6e1
CAROL SANCHES <135077050+CarolinaSanches24@users.noreply.github.com>
2024-07-21 20:07:40 -0300

    :bug: fix: App.tsx duplicate products

--- httpReact/src/App.tsx
+++ httpReact/src/App.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { VStack, HStack, Input, Button, Text } from '@chakra-ui/react';
+import React, { useState, useEffect } from 'react';
+import { VStack, HStack, Input, Button, Text, Spinner } from '@chakra-ui/react';
 import './App.css'; 
 import { Product } from './entity/product';
 import { useFetch } from './hooks/useFetch'; 
@@ -9,19 +9,27 @@ const url = "http://localhost:3000/products";
 function App() {
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
-  const { data: items = [], httpConfig } = useFetch(url);
+  const { data: items = [], httpConfig, loading } = useFetch(url);
+  const [products, setProducts] = useState<Product[]>([]);
 
-  
+  useEffect(() => {
+    if (items) {
+      setProducts(items);
+    }
+  }, [items]);
+
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    const newProduct= {
-      id:items ? items.length + 1 : 1,
+    const newProduct = {
+      id: products.length + 1, 
       name,
       price: parseFloat(price)
     };
 
     httpConfig(newProduct, "POST");
+
+    
 
     setName("");
     setPrice("");
@@ -30,15 +38,29 @@ function App() {
   return (
     <VStack spacing={4} padding={8} align="stretch" className='App'>
       <Text fontSize="2xl">Lista de Produtos</Text>
-      <VStack align="stretch">
-        {items && items.length > 0 ? (
-          items.map((product: Product) => (
-            <Text key={product.id}>{product.name} - R$: {product.price}</Text>
-          ))
-        ) : (
-          <Text>Nenhum produto encontrado</Text>
-        )}
-      </VStack>
+      {loading && (
+        <HStack>
+          <Text>Carregando dados...</Text>
+          <Spinner
+            thickness='2px'
+            speed='1.5s'
+            emptyColor='gray.200'
+            color='blue.500'
+            size='md'
+          />
+        </HStack>
+      )}
+      {!loading && (
+        <VStack align="stretch">
+          {products.length > 0 ? (
+            products.map((product: Product) => (
+              <Text key={product.id}>{product.name} - R$: {product.price}</Text>
+            ))
+          ) : (
+            <Text>Nenhum produto encontrado</Text>
+          )}
+        </VStack>
+      )}
       <VStack align="stretch" className='add-product'>
         <form onSubmit={handleSubmit}>
           <HStack>
--- httpReact/src/hooks/useFetch.tsx
+++ httpReact/src/hooks/useFetch.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Product } from "../entity/product";
 
 type ConfigData = Record<string, unknown>;
 
@@ -12,22 +13,23 @@ type FetchConfig = {
     body: string;
 } | null;
 
-export const useFetch = (url:string) => {
-    const [data, setData] = useState<null>(null);
+export const useFetch = (url: string) => {
+    const [data, setData] = useState<Product[] | null>(null);
     const [config, setConfig] = useState<FetchConfig>(null);
     const [method, setMethod] = useState<Method | null>(null);
     const [callFetch, setCallFetch] = useState(false);
-    
-    const httpConfig = (data:ConfigData, method:Method) => {
+
+    const [loading, setLoading] = useState(false);
+
+    const httpConfig = async (data: ConfigData, method: Method) => {
         if (method === "POST") {
             setConfig({
                 method: "POST",
-                 headers: {
+                headers: {
                     "Content-Type": "application/json",
                 },
                 body: JSON.stringify(data),
-      });
-
+            });
 
             setMethod(method);
             setCallFetch(true);
@@ -36,12 +38,18 @@ export const useFetch = (url:string) => {
 
     useEffect(() => {
         const fetchData = async () => {
+            setLoading(true);
             try {
                 const res = await fetch(url);
                 const json = await res.json();
                 setData(json);
+
+                setTimeout(() => {
+                    setLoading(false);
+                }, 2000);
             } catch (error) {
                 console.error("Error fetching data:", error);
+                setLoading(false);
             }
         };
 
@@ -54,7 +62,7 @@ export const useFetch = (url:string) => {
                 try {
                     const res = await fetch(url, config);
                     const json = await res.json();
-                    setData(json);
+                    return json;
                 } catch (error) {
                     console.error("Error making POST request:", error);
                 } finally {
@@ -63,8 +71,10 @@ export const useFetch = (url:string) => {
             }
         };
 
-        httpRequest();
+        if (method === "POST" && config) {
+            httpRequest();
+        }
     }, [config, method, url]);
 
-    return { data, httpConfig };
+    return { data, httpConfig, loading };
 };
